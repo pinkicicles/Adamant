5efb3734ffd688fed4bfa55aa87d9186b01b96b9
Connie <connie_p_wu@yahoo.com>
2014-04-08 12:55:41 -0700

    Initial Commit

---
+++ Adamant/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Adamant
+//
+//  Created by Connie Wu on 4/8/14.
+//  Copyright (c) 2014 Connie Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CPWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CPWAppDelegate class]));
+    }
+}
